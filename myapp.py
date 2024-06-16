c3ef4e124129fc07049b9ff90b73ab4be142735c
Nima <nalizadeh7054@gmail.com>
2024-06-16 21:05:28 +0330

    routing added

--- myapp.py
+++ myapp.py
@@ -1,15 +1,45 @@
 from webob import Request, Response
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
     
     def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
+        for path, handler in self.routes.items():
+            if path == request.path:
+                response = handler(request)
+                return response
+        response = self.default_response(request)
+        return response
+        
+    def default_response(self, request):
         response = Response()
-        response.text = f"Hello, {user_agent}"
+        response.status_code = 404
+        response.text = "Page Not Found"
         return response
     
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
+
+    
 app = API()
+
+@app.route("/")
+def index(req):
+    response = Response()
+    response.text = "Hi, from Index Page"
+    return response
+
+@app.route("/about")
+def about(req):
+    response = Response()
+    response.text = "Hi, from About Page"
+    return response
